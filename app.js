d16132fbf5cfffc46313a5f30307e21101a0281e
Velsedav <velasquez.sebastien@gmail.com>
2024-12-04 17:29:58 +0100

    JavaScript_Presque_Fini

--- app.js
+++ app.js
@@ -6,14 +6,69 @@ fetch("data.json")
   .then((dataBase) => {
     //Ensuite j'ai acces ici à ma data
     console.log(dataBase);
-    // Ici j'ai la liste de recettes
+    // Ici je vois toute ma base de données en console
     // Puis ci-dessous Je boucle sur le teableau de données
-    dataBase.forEach((dataPatisserie) => {
-      display(dataPatisserie);
-    });
+    afficher(dataBase.services, dataBase.produits, dataBase.temoignages, dataBase.avantagesClients, dataBase.texteAppelAction);
   });
 
-  function afficher(dataPatisserieActuelle) {
-    console.log(dataPatisserieActuelle.temoignages);
+// Rôle : Afficher le contenu du data.JSON
+// Paramètres : Les objets de services
+//  Retourne : Rien car elle affiche
+
+// dataBase.service, dataBase.produits, dataBase.temoignages sont des tableaux
+function afficher(services, produits, temoignages, avantagesClients) {
+  let variableServices = "";
+  services.forEach((laDataServices) => {
+    variableServices += `
     
+    ${laDataServices.nom}
+    
+    ${laDataServices.description}
+    `;
+  });
+  // console.log(variableServices);
+  
+  let variableProduits = "";
+  produits.forEach((laDataProduits) => {
+    variableProduits += `
+    ${laDataProduits.nom}
+    
+    ${laDataProduits.description}
+    
+    `;
+  });
+  // console.log(variableProduits);
+  
+  let variableTemoignages = "";
+  temoignages.forEach((laDataTemoignages) => {
+    variableTemoignages += `${laDataTemoignages.prenom}
+    ${laDataTemoignages.typeExperience}
+    ${laDataTemoignages.commentaire}
+    ${laDataTemoignages.note}
+
+    `;
+  });
+  // console.log(variableTemoignages);
+  
+  let variableAvantagesClients = `
+  
+  ${avantagesClients[0]}
+  
+  ${avantagesClients[1]}
+
+  ${avantagesClients[2]}
+
+  `;
+  for (let i = 0; i < avantagesClients.length; i++) {
+    const element = variableAvantagesClients[i];
   }
+  // console.log(variableAvantagesClients);
+
+  // let variableTexteAppelAction = `${dataBase.texteAppelAction[0]}`;
+  // console.log(variableTexteAppelAction);
+  console.log(database.texteAppelAction);
+  
+  
+  
+  
+}
